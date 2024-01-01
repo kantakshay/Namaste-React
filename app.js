4d552d6cc62caedd56f5b9fed7919a8059da69df
Akshay <akshaykant5006@gmail.com>
2024-01-01 21:34:58 +0530

    composing component

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+function Header() {
+  return (
+    <>
+      <div className="container">
+        <div className="logo"></div>
+        <div className="navbar">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading1" }, "heading 1"),
-    React.createElement("h2", { id: "heading2" }, "heading2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading1" }, "heading 1"),
-    React.createElement("h2", { id: "heading2" }, "heading2"),
-  ]),
-]);
+function CardComponent() {
+ return( <div>
+  <div>
+    <img src="https://rukminim2.flixcart.com/image/612/612/khz693k0-0/pendrive/pendrive/i/r/x/v236w-hp-original-imafxvf93gh7axat.jpeg?q=70" alt="" />
+  </div>
+  <div>
+    <h2>Namaste</h2>
+  </div>
+</div>)
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <CardComponent/>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<App />);
