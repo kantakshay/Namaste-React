45feac35218463716a96a6bc83aa2b80a3f8d3be
Akshay <akshaykant5006@gmail.com>
2024-01-13 23:19:12 +0530

    Episode 7 finding the path completed

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ const appRoute=createBrowserRouter([
         path:"/contact",
         element:<Contact/>
       },{
-        path:"/restaurats/:resid",
+        path:"/restaurants/:resid",
         element:<RestaurantMenu/>
       }
     ],
